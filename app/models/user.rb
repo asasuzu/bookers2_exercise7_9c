bd0b7deb137c91763b0fd3da94e55c11bd7a5eaa
asasuzu <133328222+asasuzu@users.noreply.github.com>
2023-07-23 12:32:49 +0000

    add_groupjoin

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
 
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :group_users
 
   
   has_one_attached :profile_image
